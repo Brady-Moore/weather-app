3d27a9f29896681941f89fb4ccd1dbf0950ee8f6
Bradoshado <bradoshado@gmail.com>
2025-03-14 00:09:47 +0900

    removed console.log's

--- src/app/components/weather-panel.tsx
+++ src/app/components/weather-panel.tsx
@@ -6,13 +6,10 @@ export default async function WeatherPanel() {
       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/tallahassee?unitGroup=us&key=${key}&contentType=json`
     );
     if (!response.ok) {
-      console.log("if block");
       return <div>{await response.text()}</div>;
     }
     data = await response.json();
   } catch (error: any) {
-    console.log("catch block");
-    console.log(error.message + error.name);
     return (
       <div>
         There was an error processing the weather data from Visual Crossing.
