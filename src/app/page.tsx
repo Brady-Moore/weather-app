0c32eb2302cf78b83bf47046c5601bcce59454b4
Bradoshado <bradoshado@gmail.com>
2025-03-19 20:02:14 +0900

    fix searching empty input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,9 @@
-import {
-  getWeatherData,
-  WeatherDataResponse,
-} from "./visual-crossing/visual-crossing-api";
+import { getWeatherData } from "./visual-crossing/visual-crossing-api";
 import SearchBar from "./components/SearchBar";
 import WeatherDisplay from "./components/WeatherDisplay";
-import { useParams } from "next/navigation";
 import { visualCrossingSampleData } from "./test/sample-data";
 import SampleDataLink from "./components/SampleDataLink";
 import ErrorMessage from "./components/ErrorMessage";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-import { useRouter } from "next/router";
-
-/* export const getServerSideProps = (async (context: any) => {
-  //TODO: get city from query
-  const city = context.query.city;
-
-  const weatherDataResponse = await getWeatherData(city);
-
-  return { props: { weatherDataResponse } };
-}) satisfies GetServerSideProps<{
-  weatherDataResponse: WeatherDataResponse;
-}>; */
 
 export default async function Home({
   searchParams,
@@ -31,7 +14,7 @@ export default async function Home({
   const weatherDataResponse =
     mode === "sample"
       ? { success: true, data: visualCrossingSampleData }
-      : typeof city === "string"
+      : typeof city === "string" && city
         ? await getWeatherData(city)
         : undefined;
   return (
