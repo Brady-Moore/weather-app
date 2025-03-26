a71ea660f3a502425dfb22c6fde2ccd5a54228f0
Bradoshado <bradoshado@gmail.com>
2025-03-27 06:53:04 +0900

    fix number error

--- src/app/components/PressureCard.test.tsx
+++ src/app/components/PressureCard.test.tsx
@@ -1,9 +1,10 @@
 import { roundNumber } from "../util/math";
 import { render, screen } from "@testing-library/react";
+import PressureCard from "./PressureCard";
 
 describe("PressureCard", () => {
   test("displays atmospheric pressure properly", () => {
-    render(<PressureCard pressure={999.7} />);
+    render(<PressureCard pressure={999.3} />);
     expect(screen.getByText("999 hPa")).toBeInTheDocument();
   });
 
