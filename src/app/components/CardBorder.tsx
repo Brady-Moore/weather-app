ca1b22e6ddb8539c1bfd206a6e4d7b41aa77c0d3
Bradoshado <bradoshado@gmail.com>
2025-03-27 06:56:52 +0900

    change margin

--- src/app/components/CardBorder.tsx
+++ src/app/components/CardBorder.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function CardBorder(props: { children: ReactNode }) {
   return (
-    <div className="rounded-md border-2 border-blue-400 my-10 p-5">
+    <div className="rounded-md border-2 border-blue-400 my-5 p-5">
       {props.children}
     </div>
   );
