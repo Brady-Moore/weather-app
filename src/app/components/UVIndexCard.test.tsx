5d85f12f909d04ac4f79c7b9ea0f0bb886d4ffba
Bradoshado <bradoshado@gmail.com>
2025-03-27 06:33:57 +0900

    add UVIndex tests

---
+++ src/app/components/UVIndexCard.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import UVIndexCard from "./UVIndexCard";
+
+describe("UVIndex", () => {
+  test("displays UV index properly", () => {
+    render(<UVIndexCard uvindex={1} />);
+    expect(screen.getByText("1")).toBeInTheDocument();
+    expect(screen.getByText("Low")).toBeInTheDocument();
+  });
+
+  test("displays UV icon", () => {
+    render(<UVIndexCard uvindex={2} />);
+    expect(screen.getByTestId("uv-index-icon")).toBeInTheDocument();
+  });
+});
