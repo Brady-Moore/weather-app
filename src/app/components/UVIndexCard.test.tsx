3a34b718b903eb4e9a3f4a131be7ef4ff100ec13
Bradoshado <bradoshado@gmail.com>
2025-03-27 16:58:26 +0900

    add UVIndex test case Moderate/High

--- src/app/components/UVIndexCard.test.tsx
+++ src/app/components/UVIndexCard.test.tsx
@@ -2,10 +2,20 @@ import { render, screen } from "@testing-library/react";
 import UVIndexCard from "./UVIndexCard";
 
 describe("UVIndexCard", () => {
-  test("displays UV index properly", () => {
+  test("displays UV index and 'Low' when UV Index", () => {
     render(<UVIndexCard uvindex={1} />);
     expect(screen.getByText("1")).toBeInTheDocument();
     expect(screen.getByText("Low")).toBeInTheDocument();
+  });
+  test("displays 'Moderate' when 3 < UV Index < 6", () => {
+    render(<UVIndexCard uvindex={3} />);
+    expect(screen.getByText("3")).toBeInTheDocument();
+    expect(screen.getByText("Moderate")).toBeInTheDocument();
+  });
+  test("displays UV index properly", () => {
+    render(<UVIndexCard uvindex={6} />);
+    expect(screen.getByText("6")).toBeInTheDocument();
+    expect(screen.getByText("High")).toBeInTheDocument();
   });
 
   test("displays UV icon", () => {
