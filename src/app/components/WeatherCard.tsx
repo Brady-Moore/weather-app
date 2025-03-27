7d11612cca8963abcc143ab072eb1ce39942ae1a
Brady Moore <140730015+Brady-Moore@users.noreply.github.com>
2025-03-27 15:22:11 +0900

    Update src/app/components/WeatherCard.tsx
    Co-authored-by: ben <bennord@gmail.com>

--- src/app/components/WeatherCard.tsx
+++ src/app/components/WeatherCard.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import { IconType } from "rocketicons";
 import CardBorder from "./CardBorder";
 
-interface WeatherCardProps {
+export interface WeatherCardProps {
   icon: ReactNode;
   title: string;
   info: number | string;
