e0d4169ca2105cda3127db94bc361744a9a70a10
bennord <bennord@gmail.com>
2025-03-27 16:29:50 +0900

    autosuggest tests

--- src/app/components/SearchBar.test.tsx
+++ src/app/components/SearchBar.test.tsx
@@ -36,4 +36,112 @@ describe("Search Bar", () => {
     screen.getByRole("searchbox");
     screen.getByRole("button");
   });
+
+  test("renders autosuggest when query length >= 2 is entered", async () => {
+    const { queryByText, getByRole } = render(<SearchBar />);
+    const searchBox = getByRole("searchbox") as HTMLInputElement;
+    expect(queryByText("Seattle, US")).toBeFalsy();
+    await userEvent.type(searchBox, "S");
+    expect(queryByText("Seattle, US")).toBeFalsy();
+    await userEvent.type(searchBox, "e");
+    expect(queryByText("Seattle, US")).toBeTruthy();
+    await userEvent.type(searchBox, "a");
+    expect(queryByText("Seattle, US")).toBeTruthy();
+  });
+
+  test("renders autosuggest with limited count", async () => {
+    const { queryByText, getByRole } = render(
+      <SearchBar
+        autoSuggestLimit={1}
+        autoSuggestSort="population"
+        autoSuggestSortDesc
+      />
+    );
+    const searchBox = getByRole("searchbox") as HTMLInputElement;
+    await userEvent.type(searchBox, "Sea");
+    expect(queryByText("Seattle, US")).toBeTruthy();
+    expect(queryByText("Southend-on-Sea, GB")).toBeFalsy();
+  });
+
+  test("renders autosuggest with ascending name order by default", async () => {
+    const { queryByText, getByRole } = render(
+      <SearchBar autoSuggestLimit={1} />
+    );
+    const searchBox = getByRole("searchbox") as HTMLInputElement;
+    await userEvent.type(searchBox, "Sea");
+    expect(queryByText("Bexhill-on-Sea, GB")).toBeTruthy();
+    expect(queryByText("Seattle, US")).toBeFalsy();
+  });
+
+  test("renders autosuggest with ascending name order", async () => {
+    const { queryByText, getByRole } = render(
+      <SearchBar autoSuggestLimit={1} autoSuggestSort="name" />
+    );
+    const searchBox = getByRole("searchbox") as HTMLInputElement;
+    await userEvent.type(searchBox, "Sea");
+    expect(queryByText("Bexhill-on-Sea, GB")).toBeTruthy();
+    expect(queryByText("Seattle, US")).toBeFalsy();
+  });
+
+  test("renders autosuggest with ascending asciiname order", async () => {
+    const { queryByText, getByRole } = render(
+      <SearchBar autoSuggestLimit={1} autoSuggestSort="asciiname" />
+    );
+    const searchBox = getByRole("searchbox") as HTMLInputElement;
+    await userEvent.type(searchBox, "Sea");
+    expect(queryByText("Bexhill-on-Sea, GB")).toBeTruthy();
+    expect(queryByText("Seattle, US")).toBeFalsy();
+  });
+
+  test("renders autosuggest with ascending population order", async () => {
+    const { queryByText, getByRole } = render(
+      <SearchBar autoSuggestLimit={1} autoSuggestSort="population" />
+    );
+    const searchBox = getByRole("searchbox") as HTMLInputElement;
+    await userEvent.type(searchBox, "Sea");
+    expect(queryByText("Seagoville, US")).toBeTruthy();
+    expect(queryByText("Seattle, US")).toBeFalsy();
+  });
+
+  test("renders autosuggest with descending population order", async () => {
+    const { queryByText, getByRole } = render(
+      <SearchBar
+        autoSuggestLimit={1}
+        autoSuggestSort="population"
+        autoSuggestSortDesc
+      />
+    );
+    const searchBox = getByRole("searchbox") as HTMLInputElement;
+    await userEvent.type(searchBox, "Sea");
+    expect(queryByText("Seattle, US")).toBeTruthy();
+    expect(queryByText("Seagoville, US")).toBeFalsy();
+  });
+
+  test("renders autosuggest with descending name order", async () => {
+    const { queryByText, getByRole } = render(
+      <SearchBar
+        autoSuggestLimit={1}
+        autoSuggestSort="name"
+        autoSuggestSortDesc
+      />
+    );
+    const searchBox = getByRole("searchbox") as HTMLInputElement;
+    await userEvent.type(searchBox, "Sea");
+    expect(queryByText("Southend-on-Sea, GB")).toBeTruthy();
+    expect(queryByText("Seattle, US")).toBeFalsy();
+  });
+
+  test("renders autosuggest with descending asciiname order", async () => {
+    const { queryByText, getByRole } = render(
+      <SearchBar
+        autoSuggestLimit={1}
+        autoSuggestSort="asciiname"
+        autoSuggestSortDesc
+      />
+    );
+    const searchBox = getByRole("searchbox") as HTMLInputElement;
+    await userEvent.type(searchBox, "Sea");
+    expect(queryByText("Southend-on-Sea, GB")).toBeTruthy();
+    expect(queryByText("Seattle, US")).toBeFalsy();
+  });
 });
